74e9d50db927613b21bcae8a18a27117f036d5a5
kantiprasanna <kantiprasanna3@gmail.com>
2021-07-03 19:08:06 +0530

    Added functionality to button

--- src/Components/Items/AddItem.js
+++ src/Components/Items/AddItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,16 +6,29 @@ import Button from '../UI/Button';
 import classes from './Items.Module.css';
 
 const AddItem = (props) => {
+    const [enteredItemName, setEnteredItemName] = useState('');
+    const [enteredQuantity, setEnteredQuantity] = useState('');
+
     const addItemHandler = (event) => {
         event.preventDefault();
+        console.log(enteredItemName, enteredQuantity);
     };
+
+    const itemNameChangeHandler = (event) => {
+        setEnteredItemName(event.target.value);
+    };
+
+    const quantityChangeHandler = (event) => {
+        setEnteredQuantity(event.target.value);
+    };
+
     return (
         <Card className={classes.input}>
             <form onSubmit={addItemHandler}>
                 <label htmlFor="itemname">Item Name</label>
-                <input id="itemname" type="text"/>
+                <input id="itemname" type="text" onChange={itemNameChangeHandler}/>
                 <label htmlFor="quantity">Quantity</label>
-                <input id="quantity" type="number"/>
+                <input id="quantity" type="number" onChange={quantityChangeHandler}/>
                 <Button type="submit">Add Item</Button>
             </form>
         </Card>
